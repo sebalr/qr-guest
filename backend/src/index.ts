b1e87e5fc85023492e9ab45e7e8f97300ac8f26c
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2026-05-10 19:03:58 -0300

    fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,7 +38,7 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 app.use('/auth', authRouter);
